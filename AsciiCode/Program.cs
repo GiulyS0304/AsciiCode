fb0dcad60c1070a4883961fd699176058f69c196
Giulia Straccali <straccaligiulia@gmail.com>
2020-11-19 12:16:43 +0100

    Codice ASCII

--- AsciiCode/Program.cs
+++ AsciiCode/Program.cs
@@ -6,7 +6,18 @@ namespace AsciiCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci un valore numerico o alfanumerico da convertire in codice ASCII");
+            char carattere = Convert.ToChar(Console.ReadLine());
+            
+            int number = carattere;
+            // aggiunge 4 al numero decimale corrispondente della lettera iserita
+            // number = number + 4;
+
+            Console.WriteLine($"Il carattere digitato nella tabella ASCII cosrrisponde al numero: {number.ToString()}");
+            string binario = Convert.ToString(number, 2).PadLeft(8, '0');
+
+            Console.WriteLine($"Il carattere in codice binario corrisponde alla sequenza di bit {binario}");
+            Console.ReadKey();
         }
     }
 }
